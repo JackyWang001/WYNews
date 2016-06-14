18c912c3d9a373896d677a9842b36c6672f3ae85
胖胖 <244557822@qq.com>
2016-06-14 14:58:48 +0800

    建立基本的数据

---
+++ WYNews/WYNews/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  WYNews
+//
+//  Created by 王建科 on 16/6/14.
+//  Copyright © 2016年 第一小组. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ WYNews/WYNews/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  WYNews
+//
+//  Created by 王建科 on 16/6/14.
+//  Copyright © 2016年 第一小组. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
